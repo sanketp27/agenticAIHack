f1143c75a72486636390c2271bbf3d7233ccc204
Sanket Patil <Sanketvp1@gmail.com>
2025-09-21 15:45:03 +0530

    Remove  .env

--- agents/travel_agent/agent.py
+++ agents/travel_agent/agent.py
@@ -1,5 +1,5 @@
 from google.adk.agents import Agent
-
+import os
 from travel_concierge import prompt
 
 from travel_concierge.sub_agents.booking.agent import booking_agent
@@ -10,10 +10,16 @@ from travel_concierge.sub_agents.post_trip.agent import post_trip_agent
 from travel_concierge.sub_agents.pre_trip.agent import pre_trip_agent
 
 from travel_concierge.tools.memory import _load_precreated_itinerary
+from google.adk.runners import Runner
+from google.adk.sessions import InMemorySessionService
+from google.genai import types
 
+USER_ID = "Travel agent"
+APP_NAME="Real time Itinery generation Application"
+GEMINI_MODEL = os.getenv("GEMINI_MODEL")
 
 root_agent = Agent(
-    model="gemini-2.5-flash",
+    model= GEMINI_MODEL,
     name="root_agent",
     description="A Travel Conceirge using the services of multiple sub-agents",
     instruction=prompt.ROOT_AGENT_INSTR,
@@ -27,3 +33,21 @@ root_agent = Agent(
     ],
     before_agent_callback=_load_precreated_itinerary,
 )
+
+
+async def setup_session_and_runner():
+    session_service = InMemorySessionService()
+    session = await session_service.create_session(app_name=APP_NAME, user_id=USER_ID, session_id=SESSION_ID)
+    runner = Runner(agent=root_agent, app_name=APP_NAME, session_service=session_service)
+    return session, runner
+
+async def travel_agent(query, SESSION_ID):
+    content = types.Content(role='user', parts=[types.Part(text=query)])
+    session, runner = await setup_session_and_runner()
+    events = runner.run_async(user_id=USER_ID, session_id=SESSION_ID, new_message=content)
+
+    async for event in events:
+        if event.is_final_response():
+            final_response = event.content.parts[0].text
+            print("Agent Response: ", final_response)
+
